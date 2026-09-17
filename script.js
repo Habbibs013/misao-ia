93a1962ba620e8757f813e6707d8f317afc3d48d
Habbibs013 <Thiago.silvestrin@escola.pr.gov.br>
2026-09-17 10:37:47 -0300

    save

--- script.js
+++ script.js
@@ -81,6 +81,10 @@ function respostaSelecionada(opcaoSelecionada) {
     atual++;
     mostraPergunta();
 }
+function aleatorio(lista) {
+const posicao = Math.floor(Math.random()* lista.length);
+return lista[posicao];
+}
 
 function mostraResultado(){
     caixaPerguntas.textContent = "Olha só o seu perfil de atuação:";
